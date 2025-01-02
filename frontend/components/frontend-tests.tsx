bd74859cdbeace5ce519634e6103b47249063268
olyatjtaylo <162665321+olyatjtaylo@users.noreply.github.com>
2025-01-02 00:55:46 -0500

    Delete frontend directory

--- frontend/components/frontend-tests.tsx
+++
@@ -1,9 +0,0 @@
-
-import { render, screen } from '@testing-library/react';
-import App from '../src/App';
-
-test('renders main app component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/welcome/i);
-  expect(linkElement).toBeInTheDocument();
-});
